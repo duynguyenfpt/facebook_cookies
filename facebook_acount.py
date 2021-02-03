86c385ebcc2db5f9274702948b4a5381cc7304e3
unknown <hoangphucnb97@gmail.com>
2021-02-03 19:43:21 +0700

    First commit

---
+++ facebook_acount.py
@@ -0,0 +1,36 @@
+import time
+import os
+import json
+
+from selenium import webdriver
+
+
+class FacebookAccount():
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.driver = webdriver.Chrome(executable_path=str(os.path.join('.', 'resources', 'chromedriver')))
+        self.driver.get('https://www.facebook.com/login.php')
+
+    def login(self):
+        element_email = self.driver.find_element_by_id('email')
+        element_password = self.driver.find_element_by_id('pass')
+
+        element_email.send_keys(self.email)
+        element_password.send_keys(self.password)
+
+        login_button = self.driver.find_element_by_id('loginbutton')
+        login_button.click()
+
+        #wait for login success
+        time.sleep(5)
+
+    def get_cookies(self):
+        return self.driver.get_cookies()
+
+    def execute_script(self, file_path='get_token_script.txt', script=''):
+        with open(str(os.path.join('.', 'resources', file_path)), 'r') as script_file:
+            final_script = script or script_file.read()
+
+        # return final_script
+        return self.driver.execute_script(final_script) 
---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+from facebook_acount import FacebookAccount
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "Welcome"
+
+@app.route('/fetch', methods=['POST'])
+def fetch():
+    email = request.form['email']
+    password = request.form['password']
+
+    my_accont = FacebookAccount(email, password)
+    my_accont.login()
+    cookies = my_accont.get_cookies()
+    script_result = my_accont.execute_script()
+    result = {'cookies': cookies, 'script_result': script_result}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True) 
